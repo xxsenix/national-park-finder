d79c764fc692a0d1e5d3bcf8901429576a379a73
Travis Callahan <callahantravis29@gmail.com>
2018-12-09 12:08:55 -0600

    added function formatQueryParams which formats the queries

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
 // put your own value below!
 const apiKey = 'cZ9vEhKBbwxKlpp80lYWwyCuJ2AJXo09Mc5GFoEL'; 
-const searchURL = 'https://www.api.nps.gov/api/v1/parks';
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 
-// function formatQueryParams(params) {
-//   const queryItems = Object.keys(params)
-//     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-//   return queryItems.join('&');
-// }
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${key}=${params[key]}`)
+  return queryItems.join('&');
+}
 
 // function displayResults(responseJson) {
 //   // if there are previous results, remove them
@@ -31,40 +31,38 @@ const searchURL = 'https://www.api.nps.gov/api/v1/parks';
 //   $('#results').removeClass('hidden');
 // };
 
-// function getYouTubeVideos(query, maxResults=10) {
-//   const params = {
-//     key: apiKey,
-//     q: query,
-//     part: 'snippet',
-//     maxResults,
-//     type: 'video'
-//   };
-//   const queryString = formatQueryParams(params)
-//   const url = searchURL + '?' + queryString;
+function getParks(state, results=10) {
+  const params = {
+    api_key: apiKey,
+    stateCode: state,
+    limit: results
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
 
-//   console.log(url);
+  console.log(url);
 
-//   fetch(url)
-//     .then(response => {
-//       if (response.ok) {
-//         return response.json();
-//       }
-//       throw new Error(response.statusText);
-//     })
-//     .then(responseJson => displayResults(responseJson))
-//     .catch(err => {
-//       $('#js-error-message').text(`Something went wrong: ${err.message}`);
-//     });
-// }
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => console.log(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    const searchTerms = $('#js-state-options').val();
+    const searchTerms = $('#js-state-options').val().join('&stateCode=');
     const maxResults = $('#js-max-results').val();
     console.log(searchTerms);
     console.log(maxResults);
-    // getYouTubeVideos(searchTerm, maxResults);
+    getParks(searchTerms, maxResults);
   });
 }
 
