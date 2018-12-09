63c6fdb375cae2655dff5315cb49cebe8dd2ddfa
Travis Callahan <callahantravis29@gmail.com>
2018-12-09 11:35:41 -0600

    added function watchFrom to get the value of multiple states and the max results num. Also updated html to reflect this.

--- script.js
+++ script.js
@@ -1,68 +1,70 @@
 'use strict';
 
 // put your own value below!
-const apiKey = 'AIzaSyC3D_kPojON4u8bUlmWZwXBCYlG29B14W0'; 
-const searchURL = 'https://www.googleapis.com/youtube/v3/search';
+const apiKey = 'cZ9vEhKBbwxKlpp80lYWwyCuJ2AJXo09Mc5GFoEL'; 
+const searchURL = 'https://www.api.nps.gov/api/v1/parks';
 
 
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
-}
+// function formatQueryParams(params) {
+//   const queryItems = Object.keys(params)
+//     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+//   return queryItems.join('&');
+// }
 
-function displayResults(responseJson) {
-  // if there are previous results, remove them
-  console.log(responseJson);
-  $('#results-list').empty();
-  // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
-    // for each video object in the items 
-    //array, add a list item to the results 
-    //list with the video title, description,
-    //and thumbnail
-    $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
-      </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
-};
+// function displayResults(responseJson) {
+//   // if there are previous results, remove them
+//   console.log(responseJson);
+//   $('#results-list').empty();
+//   // iterate through the items array
+//   for (let i = 0; i < responseJson.items.length; i++){
+//     // for each video object in the items 
+//     //array, add a list item to the results 
+//     //list with the video title, description,
+//     //and thumbnail
+//     $('#results-list').append(
+//       `<li><h3>${responseJson.items[i].snippet.title}</h3>
+//       <p>${responseJson.items[i].snippet.description}</p>
+//       <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+//       </li>`
+//     )};
+//   //display the results section  
+//   $('#results').removeClass('hidden');
+// };
 
-function getYouTubeVideos(query, maxResults=10) {
-  const params = {
-    key: apiKey,
-    q: query,
-    part: 'snippet',
-    maxResults,
-    type: 'video'
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
+// function getYouTubeVideos(query, maxResults=10) {
+//   const params = {
+//     key: apiKey,
+//     q: query,
+//     part: 'snippet',
+//     maxResults,
+//     type: 'video'
+//   };
+//   const queryString = formatQueryParams(params)
+//   const url = searchURL + '?' + queryString;
 
-  console.log(url);
+//   console.log(url);
 
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
-}
+//   fetch(url)
+//     .then(response => {
+//       if (response.ok) {
+//         return response.json();
+//       }
+//       throw new Error(response.statusText);
+//     })
+//     .then(responseJson => displayResults(responseJson))
+//     .catch(err => {
+//       $('#js-error-message').text(`Something went wrong: ${err.message}`);
+//     });
+// }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
+    const searchTerms = $('#js-state-options').val();
     const maxResults = $('#js-max-results').val();
-    getYouTubeVideos(searchTerm, maxResults);
+    console.log(searchTerms);
+    console.log(maxResults);
+    // getYouTubeVideos(searchTerm, maxResults);
   });
 }
 
